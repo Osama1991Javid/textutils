53b30d08a86f89772d7926388725e3900ed88ece
Osama1991Javid <107409786+Osama1991Javid@users.noreply.github.com>
2022-06-28 00:00:23 +0500

    Add files via upload

---
+++ build/precache-manifest.4694da95c5f86c1c561b4c56f872921d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1cfdff2b7c01c532dcc",
+    "url": "/textutils/static/css/main.fdaf6a92.chunk.css"
+  },
+  {
+    "revision": "e1cfdff2b7c01c532dcc",
+    "url": "/textutils/static/js/main.e1cfdff2.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/textutils/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "18a9211fd047086487e8",
+    "url": "/textutils/static/js/2.18a9211f.chunk.js"
+  },
+  {
+    "revision": "249b581e5cf121412222",
+    "url": "/textutils/static/js/runtime~main.249b581e.js"
+  },
+  {
+    "revision": "2759f783b6b73d967cb31c8ad4f2d28b",
+    "url": "/textutils/index.html"
+  }
+];
